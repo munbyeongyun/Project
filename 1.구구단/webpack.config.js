d73c8512a5c80ea5473754f040027dafc6a8ea3c
munbyeongyun <dkqk1326@naver.com>
2022-04-08 17:29:12 +0900

    class 버전 끝말잇기

---
+++ 1.구구단/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development", // 실서비스: production
+  devtool: "eval", // hidden-source-map
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: {
+    app: "./client",
+  }, //입력
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: [],
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, //출력
+};

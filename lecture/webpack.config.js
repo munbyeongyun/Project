a874f0fc2c56e4d2fa1c0092f37ae683ecce0a5f
munbyeongyun <dkqk1326@naver.com>
2022-04-13 18:03:59 +0900

    웹펙 서버와 핫로딩

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,32 +1,52 @@
 const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-  name: "wordrelay-setting",
+  name: "wordrelay",
   mode: "development", // 실서비스: production
-  devtool: "eval",
+  devtool: "eval", // 실서비스: hidden-source-map
   resolve: {
     extensions: [".js", ".jsx"],
   },
 
   entry: {
     app: ["./client"],
-  }, //입력
-
+  }, // 입력
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.jsx?$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-proposal-class-properties"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR"], // browserslist
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-refresh/babel",
+          ],
         },
       },
     ],
   },
-
+  plugins: [new RefreshWebpackPlugin()],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
-  }, //출력
+    publicPath: "/dist/",
+  },
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
 };
